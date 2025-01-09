b21e60f1b9de50f9f00385abb063f2fdf4ec0f28
pycoderabhishek <pycoder.abhi@gmail.com>
2025-01-09 13:07:39 +0530

    Switched from SQLite to PostgreSQL database in settings.py

--- npgc/npgc/settings.py
+++ npgc/npgc/settings.py
@@ -78,11 +78,15 @@ WSGI_APPLICATION = 'npgc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'npgcstudents',
+        'USER': 'pycoders',
+        'PASSWORD': '3104',
+        'HOST': 'localhost',  # Aapka system ka public IP
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
