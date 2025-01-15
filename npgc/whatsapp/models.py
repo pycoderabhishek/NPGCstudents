6ab5b7bc797f614a531d13fb0566921eb7383a11
pycoderabhishek <pycoder.abhi@gmail.com>
2025-01-15 10:56:41 +0530

    updated dashboard

--- npgc/whatsapp/models.py
+++ npgc/whatsapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from user.models import User
-
+from department.models import Course,Department
 class Group(models.Model):
     GROUP_TYPES = [
         ('official', 'Official'),
@@ -10,8 +10,8 @@ class Group(models.Model):
     group_id = models.AutoField(primary_key=True)
     group_name = models.CharField(max_length=255)
     semester = models.CharField(max_length=20)
-    course_id = models.IntegerField()  # You can replace IntegerField with ForeignKey if there's a Course model
-    department_id = models.IntegerField()  # Replace with ForeignKey if there's a Department model
+    course_id = models.ForeignKey(Course,  on_delete=models.CASCADE)  # You can replace IntegerField with ForeignKey if there's a Course model
+    department_id = models.ForeignKey(Department,  on_delete=models.CASCADE)  # Replace with ForeignKey if there's a Department model
     added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # Tracks who added the group
     group_link = models.URLField(max_length=500, blank=True, null=True)  # Optional field for group links
     created_at = models.DateTimeField(auto_now_add=True)  # Automatically sets timestamp on creation
--- npgc/whatsapp/views.py
+++ npgc/whatsapp/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from user.models import Teacher, Student
+from .models import Group
 from django.contrib import messages
 from taksdata.models import Event,Assignment,Notification
 # Login View
 # Create your views here.
-
+@login_required
 def whatsapp_home(request):
-    return render(request,'whatsapp/dashboard.html')
+    user = request.user.username
+    groups=Group.objects.all()
+    
+    return render(request,'whatsapp/dashboard.html',{"groups":groups})
